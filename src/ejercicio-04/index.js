d436d8d05568291075d014c3500e0f34e8435a56
Josareta <josareta@gmail.com>
2023-10-10 18:54:30 +0200

    Añadir Ejercicio 04

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Manejo de errores con Promises
+const miPromiseRechazada = new Promise ((resolver, rechazar) => {
+    setTimeout (() => {
+        rechazar ("Promesa rechazada")
+    }, 2000)
+})
+
+miPromiseRechazada.catch((error) => {
+    console.log("Error:", error)
+})
