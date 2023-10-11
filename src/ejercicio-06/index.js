adbc1b531aa4a8a665aec67f94086c5bd6895c0e
Josareta <josareta@gmail.com>
2023-10-11 20:15:39 +0200

    Añadir Ejercicio 06

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Llamada a una API con fetch()
+
+fetch("https://www.marca.com/")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  })
