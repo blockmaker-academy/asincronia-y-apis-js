dfaf0077426c54724e0b55d066fd8c6b421f89ba
Josareta <josareta@gmail.com>
2023-10-11 20:24:01 +0200

    Añadir Ejercicio 07

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,11 @@
-// Escribe tu código aquí:
+// Llamada a una API con Axios
+const axios = require('axios')
+
+axios
+  .get('https://www.marca.com')
+  .then((response) => {
+    console.log(response.data)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
