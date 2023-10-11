00766ee8c54c76de474294ecfc2f801aa259e3c4
Josareta <josareta@gmail.com>
2023-10-11 19:29:14 +0200

    Añadir Ejercicio 06

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,24 @@
-// Escribe tu código aquí:
+// Encadenamiento de Promises
+var suma = 0
+function crearPromiseConRetraso(valor, retraso) {
+    suma += valor 
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(valor)
+    }, retraso)
+  })
+}
+
+crearPromiseConRetraso(1, 1000)
+  .then((resultado1) => {
+    console.log(resultado1)
+    return crearPromiseConRetraso(2, 1000) 
+  })
+  .then((resultado2) => {
+    console.log(resultado2)
+    return crearPromiseConRetraso(3, 1000)
+  })
+  .then((resultado3) => {
+    console.log(resultado3)
+    console.log('Suma:', suma)
+})
