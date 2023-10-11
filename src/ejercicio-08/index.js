48310a398f911bfd47fed35b9ed7e57155610f00
Josareta <josareta@gmail.com>
2023-10-11 20:31:00 +0200

    Añadir Ejercicio 08

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Manipulación de datos de una API
+fetch('https://as.com/')
+  .then((response) => response.json())
+  .then((data) => {
+    const filteredPosts = data.filter((post) => post.title.startsWith('B'))
+    console.log(filteredPosts)
+  })
