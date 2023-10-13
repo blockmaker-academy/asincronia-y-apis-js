ac010c1a8a5cd27ae81b58a82c83d0ef95128763
Josareta <josareta@gmail.com>
2023-10-13 11:48:51 +0200

    Añadir Ejercicio 09

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,14 @@
-// Escribe tu código aquí:
+// Async/await con llamadas a APIs
+const axios = require('axios')
+
+async function obtenerDatosDeAPI() {
+  try {
+    const response = await axios.get('https://www.deia.eus')
+    const data = response.data
+    console.log(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+obtenerDatosDeAPI()
