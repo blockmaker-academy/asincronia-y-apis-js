4b5dee896f5907e4fdab0d39c5ec6b16b50b382b
A05k-Mikhael-luX <anastasiayanastasia.tarot@gmail.com>
2023-11-09 19:06:57 +0100

    Añadir commitEjercicio-04

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,13 @@
+const { error } = require("console")
+
 // Escribe tu código aquí:
+const mi promiseRechazada = new Promise
+((resolve,reject)=> {
+    setTimeout(()=> {
+        reject('Promise rechazada')
+    },2000)
+})
+
+miPromiseRechazada.catch((error) => {
+    console.log('Error:',error)
+})
