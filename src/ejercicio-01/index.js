98dddc632b068c076c679ce30bfee6e145b637e7
A05k-Mikhael-luX <anastasiayanastasia.tarot@gmail.com>
2023-11-09 18:41:34 +0100

    Añadir commit Ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+function imprimirHolaMundoConCallback(callback) {
+   
+    setTimeout(function () {
+        callback('Hola, mundo') 
+    }, 2000)
+}
+
+imprimirHolaMundoConCallbacK(function (mensaje) {        
+        console.log(mensaje)
+})
