838dc7fe37416da15f34d78bacb24960c320e887
isapsanch <isabelpsanch@hotmail.com>
2023-11-10 17:47:05 +0100

    Añadir ejercicio 08

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((data) => {
+    const filteredPosts = data.filter((post) => post.title.startsWith('A'))
+    console.log(filteredPosts)
+  })
