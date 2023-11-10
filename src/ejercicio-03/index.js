73b728f5636b0401d5cff122e60d877fe6cd57e9
isapsanch <isabelpsanch@hotmail.com>
2023-11-10 17:07:50 +0100

    añadir ejercicio 03git

--- src/ejercicio-03/index.js
+++ src/ejercicio-03/index.js
@@ -1,1 +1,11 @@
-// Escribe tu código aquí:
+async function operacionAsincronica() {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    return 'Operación completada'
+  }
+  
+  async function ejecutarOperacion() {
+    const resultado = await operacionAsincronica()
+    console.log(resultado)
+  }
+  
+  ejecutarOperacion()
