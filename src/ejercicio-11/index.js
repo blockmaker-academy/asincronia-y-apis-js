e5f2a7f489ce04dc0633304e1b8c86582f4e3844
A05k-Mikhael-luX <anastasiayanastasia.tarot@gmail.com>
2023-11-10 18:24:34 +0100

    Añadir commit Ejercicio-11

--- src/ejercicio-11/index.js
+++ src/ejercicio-11/index.js
@@ -1,1 +1,11 @@
+const { promises } = require("dns");
+const { resolve } = require("path");
+
 // Escribe tu código aquí:
+const promise1 = new Promise((resolve)=>setTimeout(()=> resolve(1),1000))
+const promise2 = new Promise((resolve,reject)=>setTimeout(()=>reject('Error en promise2'),2000))
+const promise3 = new Promise((resolve)=> setTimeout(() => resolve(3),1500))
+
+Promise.allSettled([promise1,promise2,promise3]).then((resultados)=> {
+    console.log('Estado de todas las promises:', resultados)
+})
