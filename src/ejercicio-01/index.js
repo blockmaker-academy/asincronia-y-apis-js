78fd55f62a693e81dedc82501072bd8513c6219b
isabelpsanch <isabelparraehs@gmail.com>
2023-11-10 16:13:21 +0100

    Añadir Ejercicio 01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+function imprimirHolaMundoConCallback(callback) {
+    setTimeout(function () {
+      callback('Hola, mundo')
+    }, 2000)
+  }
+  
+  imprimirHolaMundoConCallback(function (mensaje) {
+    console.log(mensaje)
+  })
+  
