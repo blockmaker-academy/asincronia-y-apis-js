69f56a526dda247f17ec3f4a83fed9d24395e511
isapsanch <isabelpsanch@hotmail.com>
2023-11-10 17:26:44 +0100

    Añadir ejercicio 05

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,30 @@
 // Escribe tu código aquí:
+function crearPromiseConRetraso(valor, retraso) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(valor)
+      }, retraso)
+    })
+  }
+  // Inicializamos 3 variables sin asignarle valor.
+  let numero1
+  let numero2
+  let numero3
+  
+  crearPromiseConRetraso(1, 1000)
+    .then((resultado1) => {
+      console.log(resultado1)
+      numero1 = resultado1
+      return crearPromiseConRetraso(2, 1000)
+    })
+    .then((resultado2) => {
+      console.log(resultado2)
+      numero2 = resultado2
+      return crearPromiseConRetraso(3, 1000)
+    })
+    .then((resultado3) => {
+      console.log(resultado3)
+      numero3 = resultado3
+      const suma = numero1 + numero2 + numero3
+      console.log('Suma:', suma)
+    })
