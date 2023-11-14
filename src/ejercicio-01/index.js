f428401e022cf583a61017ea53b27ff113faf9a1
Fernando <fernando30041972@hotmail.com>
2023-11-14 19:44:22 +0100

    Añadir ejercicio 01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+function imprimirHolaMundoAsyncrono(callback) {
+    setTimeout(() => {
+        callback('Hola Mundo')
+    }, 2000)
+}
+
+imprimirHolaMundoAsyncrono((mensaje) => {
+    console.log(mensaje)
+
+})
