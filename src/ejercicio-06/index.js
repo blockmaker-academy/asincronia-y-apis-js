1715eb35f2c5b3c7fcccaf36dd78f42fe9ebe4e5
epolov <epolov@gmail.com>
2023-11-27 15:12:59 +0100

    Ejercicio-06 - Llamada API con filtro

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -3,5 +3,9 @@
 fetch('https://rickandmortyapi.com/api/character')
 .then((Response) => Response.json())
 .then((data) => {
+    //Filtrar por campos devueltos en el API
+    const personajesVivos = data.results.filter((personaje) =>
+    personaje.status === 'Alive')
+
     console.log(data)
 })
