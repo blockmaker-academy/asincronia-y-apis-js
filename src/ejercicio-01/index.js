dcf8b8b15256eb7442591df8bd2057a7761e3906
epolov <epolov@gmail.com>
2023-11-27 11:49:43 +0100

    Ejercicio01 - timeout con callback

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+function imprimirHolaMundoConCallback(callback){
+    console.log('Imprimiendo Hola Mundo...')
+    setTimeout(function () {
+        callback('Hola Mundo')
+    }, 2000)
+}
+
+imprimirHolaMundoConCallback(function (mensaje) {
+    console.log(mensaje)
+})
