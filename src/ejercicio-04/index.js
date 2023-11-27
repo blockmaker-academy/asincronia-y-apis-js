eccf5aa265c843e27ce402785fe464c9a5731086
epolov <epolov@gmail.com>
2023-11-27 15:23:53 +0100

    Ejercicio-04 - Manejo de errores con promises

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,11 @@
-// Escribe tu código aquí:
+// Escribe tu código aquí: MANEJO DE ERRORES CON PROMISES
+
+const promiserechazada = new Promise((resolve, rejeact) => {
+    setTimeout(() => {
+        rejeact('PROMISE RECHAZADA!!!!')
+    }, 2000);
+})
+
+promiserechazada.catch((error) => {
+    console.log('Error:', error)
+})
