11777f1ce306cb193712d43e4f53ab8b87ab7df7
epolov <epolov@gmail.com>
2023-11-29 08:33:10 +0100

    Ejercicio-09 - PROMISELL.ALL

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,6 @@
-// Escribe tu código aquí:
+// Escribe tu código aquí: PROMISES.ALL
+
+//Crear promises
+
+const prom1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000))
+const prom2 = new Promise((relove) => )
