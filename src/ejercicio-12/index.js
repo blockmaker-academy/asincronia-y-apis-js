443068208756c7e87918e2623079545f2e8fec8f
epolov <epolov@gmail.com>
2023-12-03 18:59:39 +0100

    Ejercicio 12 - Bucle con async/await

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,11 @@
-// Escribe tu código aquí:
+// Escribe tu código aquí: EJERCICIO 12
+
+async function elementprocess(elements){
+    for (const element of elements){
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+        console.log('Procesando elemento: ', element)
+    }
+}
+
+const elements = [10,20,30,40,50]
+elementprocess(elements)
