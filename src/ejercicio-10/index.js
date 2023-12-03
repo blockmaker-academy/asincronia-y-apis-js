c1fb1561caf6e743f0ba0539ef2e5afd0183ebdb
epolov <epolov@gmail.com>
2023-12-03 18:38:08 +0100

    Ejercicio-10 - Promise.all

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -3,4 +3,9 @@
 //Crear promises
 
 const prom1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000))
-const prom2 = new Promise((relove) => )
+const prom2 = new Promise((resolve) => setTimeout(() => resolve(2),  2000))
+const prom3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000))
+
+Promise.all([prom1, prom2, prom3]).then((resultado) => {
+    console.log('Se han resuelto todas las promises:', resultado)
+})
