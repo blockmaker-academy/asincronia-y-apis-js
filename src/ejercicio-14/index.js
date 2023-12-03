3a6d826e05233989aae0c0d05b5fc05ed58e0d99
epolov <epolov@gmail.com>
2023-12-03 19:47:12 +0100

    Ejercicio-14 - manipulacion de datos

--- src/ejercicio-14/index.js
+++ src/ejercicio-14/index.js
@@ -1,1 +1,22 @@
-// Escribe tu código aquí:
+// Escribe tu código aquí: EJERCICIO 14 Encadenamiento de Promises
+
+//fetch('https://pokeapi.co/api/v2/pokemon-species/')
+//    .then((Response) => Response.json())
+//    .then((data) => {
+//        return data.map((aegislash) => name.toUpperCase())
+//    })
+//    .catch((error) => {
+//      console.error('Error:', error)
+//    })
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((data) => {
+    return data.map((post) => post.title.toUpperCase())
+  })
+  .then((titles) => {
+    console.log('Títulos en mayúsculas:', titles)
+  })
+  .catch((error) => {
+    console.error('Error:', error)
+  })
