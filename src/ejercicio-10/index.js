23ced4b3e803cd8f196f83ededd637c42a605af9
radiogio <giovannychopite@gmail.com>
2023-12-12 18:27:39 -0500

    Añadir ejercicio-10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,12 @@
-// Escribe tu código aquí:
+const promise1 = new Promise((resolve) =>
+setTimeout(() => resolve(1), 1000))
+const promise2 = new Promise((resolve) =>
+setTimeout(() =>resolve(2), 2000))
+const promise3 = new Promise((resolve) =>
+setTimeout(() => resolve(3), 1500))
+
+Promise.all([promise1, promise2,
+     promise3]).then((resultados) => {
+    console.log('todas las Promises se han resuelto:', 
+    resultados)
+})
