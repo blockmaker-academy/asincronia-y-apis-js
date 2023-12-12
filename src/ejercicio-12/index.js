30a343317acebd2ae7a5048603b7cfe3dacd85b9
radiogio <giovannychopite@gmail.com>
2023-12-12 18:59:13 -0500

    Añadir ejercicio-12

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+async function procesarElementos(elmento) {
+    for (const elmento of elementos) {
+        await new Promise((resolve) => setTimeout(resolve,
+            1000))
+            console.log('Procesando elemento:', elmento)
+    }
+}
+
+const elementos = [1,2,3,4,5]
+procesarElementos(elementos)
