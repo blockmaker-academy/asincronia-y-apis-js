877aa6fd9959a1eb21649da41d29232da178c1e6
radiogio <giovannychopite@gmail.com>
2023-12-12 19:12:06 -0500

    Añadir ejercicio-13

--- src/ejercicio-13/index.js
+++ src/ejercicio-13/index.js
@@ -1,1 +1,13 @@
-// Escribe tu código aquí:
+const axios = require('axios')
+
+async function obtenerDatoasDeAPI() {
+    try {
+        const responce = await
+        axios.get('https://jsonplaceholder.typicode.com/posts/invalid')
+        console.log(responce.data)
+    }  catch (error) {
+        console.error('Error al obtener datos:',
+        error.message)
+    }
+}
+obtenerDatoasDeAPI()
